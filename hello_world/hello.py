bde54f49c2b0f98591fabfc2ad45eb3c60bd5e14
Chris Heckler <hecklerchris@hotmail.com>
2018-06-11 14:23:14 -0700

    Created templates folder and page Return Hello World using a template

--- hello_world/hello.py
+++ hello_world/hello.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env/ python3
 # Copyright (c) 2018 Chris Heckler <hecklerchris@hotmail.com>
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!!'
+    return render_template('hello.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
