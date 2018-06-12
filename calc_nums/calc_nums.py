8f46275951bb0e1fe3a6abd4f56e8964375aff55
Chris Heckler <hecklerchris@hotmail.com>
2018-06-12 11:51:36 -0700

    Modified calc_nums.py to be more concise I removed parse_request and updated home.html for info about usage.

--- calc_nums/calc_nums.py
+++ calc_nums/calc_nums.py
@@ -1,22 +1,12 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Copyright (c) 2018 Chris Heckler <hecklerchris@hotmail.com>
 
-from flask import Flask, request, render_template
-from urllib.parse import unquote_plus
+from flask import Flask, request, render_template, abort
+from urllib import unquote_plus
 import json
 import re
 
-app = Flask(__name__, template_folder='templates')
-
-
-def parse_request(req):
-    # Parses requests body into dictionary
-    payload = req.get_data()
-    payload = unquote_plus(payload)
-    payload = re.sub('payload=', '', payload)
-    payload = json.loads(payload)
-
-    return payload
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
@@ -26,6 +16,16 @@ def index():
     """
     return (render_template('home.html'), 200, None)
 
+@app.route('/api/print', methods=['GET'])
+def print_test():
+    """
+    Sends a POST request to localhost:5000/api/print with
+    a JSON body with a and b.
+    """
+    a = request.args.get("a")
+    b = request.args.get("b")
+    return json.dumps("{'a': %s, 'b': %s}" % (a,b))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
