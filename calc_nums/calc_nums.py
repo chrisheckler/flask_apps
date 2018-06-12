90404d0cdfcfbf9137e3b95d39185ff94288000c
Chris Heckler <hecklerchris@hotmail.com>
2018-06-12 13:15:26 -0700

    Added route('/api/add') and func This returns the sum of a and b

--- calc_nums/calc_nums.py
+++ calc_nums/calc_nums.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) 2018 Chris Heckler <hecklerchris@hotmail.com>
 
-from flask import Flask, request, render_template, abort
-from urllib import unquote_plus
+from flask import Flask, request, render_template, abort, jsonify
 import json
 import re
 
@@ -16,15 +15,26 @@ def index():
     """
     return (render_template('home.html'), 200, None)
 
+
 @app.route('/api/print', methods=['GET'])
 def print_test():
     """
-    Sends a POST request to localhost:5000/api/print with
-    a JSON body with a and b.
+    Sends a request to localhost:5000/api/print and prints
+    variables in json.
     """
     a = request.args.get("a")
     b = request.args.get("b")
     return json.dumps("{'a': %s, 'b': %s}" % (a,b))
+
+
+@app.route('/api/add', methods=['GET'])
+def add_nums():
+    """
+    Sends a request and returns the sum of the variables.
+    """
+    a = request.args.get('a',0,type=int)
+    b = request.args.get('b',0,type=int)
+    return jsonify(a + b) 
 
 if __name__ == '__main__':
     app.run(debug=True)
