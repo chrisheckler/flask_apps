c693a9352c34eacb5a68474e6b50290cbc878844
Chris Heckler <hecklerchris@hotmail.com>
2018-06-15 11:00:12 -0700

    Route and func to get specific task The task id is passed into the url

--- todo.py/todo.py
+++ todo.py/todo.py
@@ -1,6 +1,6 @@
-#!flask/bin/python
+#!/usr/bin/python3
 # Copyright (c) 2018 Chris Heckler <hecklerchris@hotmail.com>
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
@@ -19,9 +19,19 @@ tasks = [
     }
 ]
 
+
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task():
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
