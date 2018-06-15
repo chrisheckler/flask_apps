7a3e001d07bbb37ce7ec2892ce0ceadd6d4e8c90
Chris Heckler <hecklerchris@hotmail.com>
2018-06-15 12:34:14 -0700

    Replaced task id with a public facing uri

--- todo.py/todo.py
+++ todo.py/todo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # Copyright (c) 2018 Chris Heckler <hecklerchris@hotmail.com>
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
 
 app = Flask(__name__)
 
@@ -20,6 +20,17 @@ tasks = [
 ]
 
 
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], 
+                                      _external = True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not Found'}), 404)
@@ -27,7 +38,7 @@ def not_found(error):
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
@@ -71,12 +82,15 @@ def delete_task(task_id):
     tasks.remove(task[0])
     return jsonify({'result': True})
 
+
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
     return jsonify({'task': task[0]})
+
+
 
 
 if __name__ == '__main__':
